e6bef7c874bcee5bf7e653eaeae911712e0cb0b2
Ajay <ajayreigns0@gmail.com>
2024-03-11 12:03:05 +0530

    async actions using redux toolkit

--- rtk-demo/features/icecream/icecreamSlice.js
+++ rtk-demo/features/icecream/icecreamSlice.js
@@ -1,3 +1,5 @@
+const { cakeActions } = require("../cake/cakeSlice");
+
 const createSlice = require("@reduxjs/toolkit").createSlice;
 
 const initialState = {
@@ -13,6 +15,11 @@ const icecreamSlice = createSlice({
     restocked: (state, action) => {
       state.numberOfIceCreams += action.payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(cakeActions.ordered, (state) => {
+      state.numberOfIceCreams--;
+    });
   },
 });
 
